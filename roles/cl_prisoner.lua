182519a020dcaaacc16cb17f531b05e9b0d5b788
Kanersps <kanersps@pm.me>
2019-01-01 21:20:51 +0100

    Attempt to fix announcer issues in the prison

--- roles/cl_prisoner.lua
+++ roles/cl_prisoner.lua
@@ -17,6 +17,13 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(500)
 
+            -- Announcer
+            TerminateAllScriptsWithThisName("re_prison")
+            ClearAmbientZoneState("AZ_COUNTRYSIDE_PRISON_01_ANNOUNCER_ALARM", 0)
+            ClearAmbientZoneState("AZ_COUNTRYSIDE_PRISON_01_ANNOUNCER_GENERAL", 0)
+            ClearAmbientZoneState("AZ_COUNTRYSIDE_PRISON_01_ANNOUNCER_WARNING", 0)
+            StopAlarm("PRISON_ALARMS", 1)
+
         if(_role == "prisoner")then
             if(not IsPedWalking(PlayerPedId()) and not acknowledgedSprinting and not IsPedStill(PlayerPedId()))then
                 TriggerServerEvent("pf_sv:sprinting", true, location)

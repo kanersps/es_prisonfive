3822fa65ec66e14159485b6050cc3bfd268aeeb0
Kanersps <kanersps@pm.me>
2019-01-03 23:19:42 +0100

    Added item purchasing for prisoners only, at the NPC in the canteen

--- systems/cl_economy.lua
+++ systems/cl_economy.lua
@@ -1,3 +1,11 @@
+RegisterNetEvent("pf_cl:purchaseComplete")
+
+function DisplayHelpText(str)
+	SetTextComponentFormat("STRING")
+	AddTextComponentString(str)
+	DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
+
 Citizen.CreateThread(function()
     RequestModel(0xb1bb9b59)
 
@@ -23,4 +31,44 @@ Citizen.CreateThread(function()
     SetPedCombatRange(dealer, 0.0)
     SetPedCombatAbility(dealer, 0)
     SetPedAlertness(dealer, 0)
+
+    while true do
+        Citizen.Wait(0)
+
+        local playerLocation = GetEntityCoords(PlayerPedId())
+        if Vdist2(playerLocation.x, playerLocation.y, playerLocation.z, 1748.09, 2591.85, 44.56) < 3.0 then
+            if(_role == "prisoner")then
+                DisplayHelpText("Press ~INPUT_CONTEXT~ to open the shop")
+
+                if(IsControlJustReleased(1, 51))then
+                    SendNUIMessage({
+                        type = "pf_ui:enableShop"
+                    })
+                    SetNuiFocus(true, true) 
+                end
+            else
+                DisplayHelpText("Get away from me, you pig!")
+            end
+        end
+    end
+end)
+
+RegisterNUICallback("disableShop", function(data)
+    SendNUIMessage({
+        type = "pf_ui:disableShop"
+    })
+    SetNuiFocus(false, false) 
+end)
+
+RegisterNUICallback("purchase", function(data)
+    TriggerServerEvent("pf_sv:purchaseItem", data.item)
+end)
+
+AddEventHandler("pf_cl:purchaseComplete", function(id)
+    if(id == "give_armor")then
+        SetPedArmour(PlayerPedId(), 100)
+    else
+        GiveWeaponToPed(PlayerPedId(), GetHashKey(id), 100, true, false)
+        SetPedInfiniteAmmo(PlayerPedId(), true, GetHashKey(id))        
+    end
 end)
--- systems/sv_economy.lua
+++ systems/sv_economy.lua
@@ -1,3 +1,5 @@
+RegisterNetEvent("pf_sv:purchaseItem")
+
 -- Payments
 Citizen.CreateThread(function()
     while true do
@@ -22,4 +24,40 @@ TriggerEvent("es:addCommand", "role", function(_source, args, user)
     user.removeMoney(1)
 
     TriggerClientEvent("pf_cl:selectNewRole", _source)
+end)
+
+-- Purchasing items
+local items = {
+    {
+        price = 60,
+        item = "WEAPON_PISTOL"
+    },
+    {
+        price = 30,
+        item = "WEAPON_KNIFE"
+    },
+    {
+        price = 200,
+        item = "WEAPON_SAWNOFFSHOTGUN"
+    },
+    {
+        price = 500,
+        item = "WEAPON_ASSAULTRIFLE"
+    },
+    {
+        price = 20,
+        item = "give_armor"
+    }
+}
+
+AddEventHandler("pf_sv:purchaseItem", function(id)
+    local _source = source
+    TriggerEvent("es:getPlayerFromId", _source, function(user)
+        if(user.getMoney() >= items[id].price)then
+            user.removeMoney(items[id].price)
+            TriggerClientEvent("pf_cl:purchaseComplete", _source, items[id].item)
+        else
+
+        end
+    end)
 end)

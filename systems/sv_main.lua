a43f9a859ad0f9fcf48e472040ffb5e6d9c6aeda
Kanersps <kanersps@pm.me>
2019-01-14 21:34:49 +0100

    Some fixes to pinfo

--- systems/sv_main.lua
+++ systems/sv_main.lua
@@ -9,9 +9,15 @@ print("es_prisonfive started")
 TriggerEvent("es:addGroupCommand", "pinfo", "admin", function(_source, args, user)
     if GetPlayerName(args[1]) then
         TriggerEvent("es:getPlayerFromId", tonumber(args[1]), function(_target)
-            print("^1[PrisonFive] ^0Player info (" .. GetPlayerName(args[1]) .. ")")
-            print("^1[PrisonFive] ^0Money: " .. user.getMoney())
-            print("^1[PrisonFive] ^0Role: " .. user.getPrisonRole())
+            if(_source ~= 0)then
+                TriggerClientEvent('chat:addMessage', source, { args = {"^1PrisonFive", "^0Player info (" .. GetPlayerName(args[1]) .. ")"}})
+                TriggerClientEvent('chat:addMessage', source, { args = {"^1PrisonFive", "^0Money: " .. _target.getMoney()}})
+                TriggerClientEvent('chat:addMessage', source, { args = {"^1PrisonFive", "^0Role: " .. _target.getPrisonRole()}})
+            else
+                print("^1[PrisonFive] ^0Player info (" .. GetPlayerName(args[1]) .. ")")
+                print("^1[PrisonFive] ^0Money: " .. _target.getMoney())
+                print("^1[PrisonFive] ^0Role: " .. _target.getPrisonRole())
+            end
         end)
     end
 end, function(source, args, user)

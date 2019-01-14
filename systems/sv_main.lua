b14c92133c213de14381a70552338ea000e02589
Kanersps <kanersps@pm.me>
2019-01-14 21:28:07 +0100

    added pinfo for admins

--- systems/sv_main.lua
+++ systems/sv_main.lua
@@ -5,3 +5,15 @@ for k,v in pairs(roles)do
 end
 
 print("es_prisonfive started")
+
+TriggerEvent("es:addGroupCommand", "pinfo", "admin", function(_source, args, user)
+    if GetPlayerName(args[1]) then
+        print("[PrisonFive] Player info (" .. GetPlayerName(args[1]) .. ")")
+
+        TriggerEvent("es:getPlayerFromId", tonumber(args[1]), function(_target)
+            print("\n[PrisonFive] Player info (" .. GetPlayerName(args[1]) .. ")")
+            print("\n[PrisonFive] Money: " .. user.getMoney())
+            print("\n[PrisonFive] Role: " .. user.getPrisonRole())
+        end)
+    end
+end)

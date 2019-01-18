5a3135ad2fb7045ecac429b127bbbecf8a9bc194
Kanersps <kanersps@pm.me>
2019-01-18 16:58:31 +0100

    Few changes/fixes

--- roles/cl_prisoner.lua
+++ roles/cl_prisoner.lua
@@ -45,3 +45,32 @@ Citizen.CreateThread(function()
         end
     end
 end)
+
+AddEventHandler("playerSpawned", function()
+    Citizen.CreateThread(function()
+        if(_role ~= "prisoner")then
+            return
+        end
+        
+        local model = GetHashKey("mp_m_freemode_01")
+    
+        RequestModel(model)
+        while not HasModelLoaded(model)do Wait(0) end
+    
+        SetPlayerModel(PlayerId(), model)
+        SetPedDefaultComponentVariation(PlayerPedId())
+        SetPedComponentVariation(PlayerPedId(), 0, 0, 0, 0)
+        SetPedComponentVariation(PlayerPedId(), 2, math.random(5), math.random(5), 0)
+    
+        if math.random(5) < 3 then
+            SetPedComponentVariation(PlayerPedId(), 8, 70, 0, 0)
+            SetPedComponentVariation(PlayerPedId(), 3, 1, 0, 0)
+            SetPedComponentVariation(PlayerPedId(), 11, 146, 7, 0)
+        else
+            SetPedComponentVariation(PlayerPedId(), 8, 15, 0, 0)
+            SetPedComponentVariation(PlayerPedId(), 11, 1, 0, 0)
+        end
+        SetPedComponentVariation(PlayerPedId(), 4, 27, 2, 0)
+        SetPedComponentVariation(PlayerPedId(), 6, 8, 0, 0)
+    end)
+end)
